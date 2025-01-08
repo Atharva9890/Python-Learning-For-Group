0cbf809cd043651d4d4cf0c41071d88c837f20cc
BrijKakadiya <87035725+BrijKakadiya@users.noreply.github.com>
2025-01-07 16:44:10 -0800

    Brij Kakadiya

--- Basics.py
+++ Basics.py
@@ -1,1 +1,85 @@
-print('Hello World')
+# print('Hello World')
+
+# # daatypes
+# # int -> 1 2 3 4 5 6 7 8 9 10
+# # float -> 1.1 2.2 3.3
+# # string -> 'Hello World'
+# # boolean -> True or False
+
+# # variables
+# x = 1
+# y = 2
+# z = 3   # this is a comment
+# print(x + y + z)
+
+# # operators
+# print(x + y)
+# print(x - y)
+# print(x * y)
+# print(x / y)
+# print(x % y)
+# print(x ** y)
+# print(x // y)
+
+# # strings
+# print('Hello World')
+# print("Hello World")
+# print('Hello "World"')
+# print("Hello 'World'")
+# print("Hello \"World\"")
+# print("Hello \nWorld")
+# print("Hello \tWorld")  
+
+# # boolean
+# print(True)
+# print(False)
+
+# # if else
+# if x > y:
+#     print('x is greater than y')
+# else:
+#     print('x is less than y')   
+
+# #if else ladder
+# if x > y:
+#     print('x is greater than y')
+# elif x < y:
+#     print('x is less than y')
+# else:
+#     print('x is equal to y')
+
+# # loops
+# while x < y:
+#     print(x)
+#     x = x + 1
+
+# for i in range(10):
+#     print(i)
+
+# for i in range(10, -1,-1):
+#     print(i)
+
+
+# for i in reversed(range(10)):
+#     print(i)
+
+# functions
+def add(x, y):
+    return x + y
+
+print(add(1, 2))
+
+def num(x):
+    if x % 2 == 0:
+        return 'even'
+    else:
+        return 'odd'
+print(num(5))
+
+#data structures
+# list
+# tuple
+# set
+# dictionary
+
+# list
